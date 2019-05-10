2dbbd676359c918431dc6ac9ced0ea9e84fa3d37
Justin Pfeifler <justin.pfeifler@revunit.com>
2019-05-10 11:34:27 -0500

    Add logic for removing 0s from grid

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import robot from './robot.png';
 import './App.css';
 import SudokuGrid from './SudokuGrid';
+import SudokuGenerate from './SudokuGenerate';
 
-const referenceGrid = [
-  [7, 9, 1, 0, 0, 2, 0, 4, 5],
-  [0, 0, 3, 4, 0, 0, 0, 2, 9],
-  [0, 0, 0, 1, 9, 5, 0, 8, 0],
-  [6, 3, 0, 0, 0, 9, 0, 1, 8],
-  [0, 0, 7, 3, 0, 0, 2, 0, 0],
-  [0, 5, 0, 0, 1, 4, 0, 3, 0],
-  [0, 7, 0, 0, 0, 0, 5, 0, 2],
-  [4, 0, 9, 0, 5, 3, 8, 0, 0],
-  [1, 0, 0, 2, 8, 0, 4, 0, 0],
-];
+// const referenceGrid = [
+//   [7, 9, 1, 0, 0, 2, 0, 4, 5],
+//   [0, 0, 3, 4, 0, 0, 0, 2, 9],
+//   [0, 0, 0, 1, 9, 5, 0, 8, 0],
+//   [6, 3, 0, 0, 0, 9, 0, 1, 8],
+//   [0, 0, 7, 3, 0, 0, 2, 0, 0],
+//   [0, 5, 0, 0, 1, 4, 0, 3, 0],
+//   [0, 7, 0, 0, 0, 0, 5, 0, 2],
+//   [4, 0, 9, 0, 5, 3, 8, 0, 0],
+//   [1, 0, 0, 2, 8, 0, 4, 0, 0],
+// ];
+
+const grid = SudokuGenerate('easy');
 
 class App extends Component {
   render() {
+
+    for (let i=0; i < grid.length; i++) {
+      for (let j=0; j < grid[i].length; j++) {
+        if (grid[i][j] === 0) {
+          grid[i][j] = ' ';
+        }
+      }
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Sudoku Solver</h1>
-          <SudokuGrid grid={ referenceGrid } />
+          <SudokuGrid grid={ grid } />
           <p>
             Generate a puzzle
           </p>
